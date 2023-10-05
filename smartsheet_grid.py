e28d686a820eb241ed7f21ac876ea9e6e5b7adc4
Ariel Vardy <77558607+ArJonVar@users.noreply.github.com>
2023-10-05 10:14:48 -0700

    drafted row posting
    has not be tested yet!

--- smartsheet_grid.py
+++ smartsheet_grid.py
@@ -38,24 +38,26 @@ class grid:
     def __init__(self, grid_id):
         self.grid_id = grid_id
         self.grid_content = None
-        self.column_df = self.get_column_df()
-    
-    def get_column_df(self):
         if self.token == None:
             return "MUST SET TOKEN"
         else:
-            smart = smartsheet.Smartsheet(access_token=self.token)
-            smart.errors_as_exceptions(True)
+            self.smart = smartsheet.Smartsheet(access_token=self.token)
+            self.smart.errors_as_exceptions(True)
+    
+    def get_column_df(self):
+        '''returns a df with data on the columns: title, type, options, etc...'''
+        if self.token == None:
+            return "MUST SET TOKEN"
+        else:
             return pd.DataFrame.from_dict(
-            (smart.Sheets.get_columns(self.grid_id, level=2, include='objectValue', include_all=True)).to_dict().get("data")
+            (self.smart.Sheets.get_columns(self.grid_id, level=2, include='objectValue', include_all=True)).to_dict().get("data")
         )
 
     def df_id_by_col(self, column_names):
+        '''I never use, name sounds like it returns an id per column'''
         if self.token == None:
             return "MUST SET TOKEN"
         else:
-            smart = smartsheet.Smartsheet(access_token=self.token)
-            smart.errors_as_exceptions(True)
             columnids = []
             col_index = []
             for col in column_names:
@@ -63,7 +65,7 @@ class grid:
                 columnids.append(col1.to_dict().get("id"))
                 col_index.append(col1.to_dict().get("index"))
             sorted_col = [x for y, x in sorted(zip(col_index, column_names))]
-            sfetch = smart.Sheets.get_sheet(self.grid_id, column_ids=columnids)
+            sfetch = self.smart.Sheets.get_sheet(self.grid_id, column_ids=columnids)
             cols = ["id"] + sorted_col
             c = []
             p = sfetch.to_dict()
@@ -76,13 +78,14 @@ class grid:
             return pd.DataFrame(c, columns=cols)
 
     def fetch_content(self):
+        '''this fetches data, ask coby why this is seperated
+        when this is done, there are now new objects created for various scenarios-- column_ids, row_ids, and the main sheet df'''
         if self.token == None:
             return "MUST SET TOKEN"
         else:
-            smart = smartsheet.Smartsheet(access_token=self.token)
-            smart.errors_as_exceptions(True)
-            self.grid_content = (smart.Sheets.get_sheet(self.grid_id)).to_dict()
+            self.grid_content = (self.smart.Sheets.get_sheet(self.grid_id)).to_dict()
             self.grid_name = (self.grid_content).get("name")
+            self.grid_url = (self.grid_content).get("permalink")
             # this attributes pulls the column headers
             self.grid_columns = [i.get("title") for i in (self.grid_content).get("columns")]
             # note that the grid_rows is equivelant to the cell's 'Display Value'
@@ -101,6 +104,8 @@ class grid:
                         else:
                             c.append(l)
                     (self.grid_rows).append(c)
+            
+            # resulting fetched content
             self.grid_rows = self.grid_rows
             if (self.grid_content).get("rows") == None:
                 self.grid_row_ids = []
@@ -109,16 +114,16 @@ class grid:
             self.grid_column_ids = [i.get("id") for i in (self.grid_content).get("columns")]
             self.df = pd.DataFrame(self.grid_rows, columns=self.grid_columns)
             self.df["id"]=self.grid_row_ids
+            self.column_df = self.get_column_df()
             
     def fetch_summary_content(self):
+        '''builds the summary df for summary columns'''
         if self.token == None:
             return "MUST SET TOKEN"
         else:
-            smart = smartsheet.Smartsheet(access_token=self.token)
-            smart.errors_as_exceptions(True)
-            self.grid_content = (smart.Sheets.get_sheet_summary_fields(self.grid_id)).to_dict()
+            self.grid_content = (self.smart.Sheets.get_sheet_summary_fields(self.grid_id)).to_dict()
             # this attributes pulls the column headers
-            self.summary_params=[ 'title','createdAt', 'createdBy', 'displayValue', 'formula', 'id', 'index', 'locked', 'lockedForUser', 'modifiedAt', 'modifiedBy', 'objectValue', 'type']
+            self.summary_params=['title','createdAt', 'createdBy', 'displayValue', 'formula', 'id', 'index', 'locked', 'lockedForUser', 'modifiedAt', 'modifiedBy', 'objectValue', 'type']
             self.grid_rows = []
             if (self.grid_content).get("data") == None:
                 self.grid_rows = []
@@ -141,9 +146,51 @@ class grid:
         if self.token == None:
             return "MUST SET TOKEN"
         else:
-            smart = smartsheet.Smartsheet(access_token=self.token)
-            smart.errors_as_exceptions(True)
             regex_string = f'[{exclusion_string}]'
             self.column_reduction =  self.column_df[self.column_df['title'].str.contains(regex_string,regex=True)==False]
             self.reduced_column_ids = list(self.column_reduction.id)
             self.reduced_column_names = list(self.column_reduction.title)
+
+    def prep_post(self, column_title_list, sheet_id):
+        '''preps for ss post
+        creating a dictionary per column:
+        { <title of column> : <column id> }
+        column_df argument is not needed if user used fetch_cotent() on sheet id already'''
+
+        # if the sheet idea matches current sheet id, grab existing column_df, else fetch it
+        if sheet_id == self.grid_id:
+            column_df = self.column_df
+        else:
+            self.grid_id = sheet_id
+            self.fetch_content()
+            column_df = self.column_df
+        
+
+        if len(column_title_list) == 0:
+            return "column name list cannot be empty"
+        else:
+            self.column_id_dict = {title: column_df.loc[column_df['title'] == title]['id'].tolist()[0] for title in column_title_list}
+    
+    def post_new_rows(self, posting_data, posting_sheet_id, post_to_top=False):
+        '''posts new row to sheet, does not account for various column types at the moment
+        posting data is a list of dictionaries, one per row, where the key is the name of the column, and the value is the value you want to post
+        then this function creates a second dictionary holding each column's id, and then posts the data one dictionary at a time (each is a row)
+        post_to_top means the new row will appear on top, else it will appear on bottom
+        TODO: if using post_to_top==False, I should really delete the empty rows in the sheet so it will properly post to bottom'''
+        column_title_list = list(self.posting_data[0].keys())
+        self.prep_post(column_title_list, posting_sheet_id)
+        
+        rows = []
+
+        for item in posting_data:
+            row = smartsheet.models.Row()
+            row.to_top = post_to_top
+            for key in self.column_id_dict:
+                if item.get(key) != None:     
+                    row.cells.append({
+                    'column_id': self.column_id_dict[key],
+                    'value': item[key]
+                    })
+            rows.append(row)
+
+        self.post_response = self.smart.Sheets.add_rows(posting_sheet_id, rows)
